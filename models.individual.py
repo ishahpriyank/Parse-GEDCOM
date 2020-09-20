b52ad743fc2e120eb33765fb86406afa2b1654d7
Priyank Shah <47760425+ishahpriyank@users.noreply.github.com>
2020-09-20 16:21:19 -0400

    Create models.individual.py

---
+++ models.individual.py
@@ -0,0 +1,34 @@
+import datetime
+from datetime import datetime, date
+from typing import Optional, Dict, List
+
+
+class Individual:
+    """ holds an Individual record """
+    def __init__(self, _id=None, name=None, sex=None, birt=None, alive=True, deat=False):
+        """ store Individual info """
+        self.id = _id
+        self.name = name
+        self.sex = sex
+        self.birt: Optional[Dict[str, str]] = birt
+        self.alive = alive
+        self.deat: Optional[bool, Dict[str, str]] = deat
+        self.famc: List[str] = []
+        self.fams: List[str] = []
+
+    def age(self):
+        """ calculate age using the birth date """
+        today = date.today()
+        birthday = datetime.strptime(self.birt['date'], "%d %b %Y")
+        age = today.year - birthday.year - \
+              ((today.month, today.day) < (birthday.month, birthday.day))
+        return age
+
+    def info(self):
+        """ return Individual info """
+        alive = True if self.deat is False else False
+        death = 'NA' if self.deat is False else self.deat['date']
+        child = 'NA' if len(self.famc) == 0 else self.famc
+        spouse = 'NA' if len(self.fams) == 0 else self.fams
+        return [self.id, self.name, self.sex, self.birt['date'],
+                self.age(), alive, death, child, spouse]
